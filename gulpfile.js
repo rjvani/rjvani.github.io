f9fd9b894ddbbdf8333b313b9fd6a61b17f6d8c9
Rohan Jadvani <rj1030@gmail.com>
2016-08-18 23:56:24 -0400

    Add watcher for less files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
 
-gulp.task('watch', () => {
-  console.log('weee');
+gulp.task('less', function() {
+  return gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
 });
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('less/*.less', ['less']);
+});
+
+gulp.task('default', ['watch']);
